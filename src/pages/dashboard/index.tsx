0c973fe2e6f900c55f1c6bfc251f446584f1db88
matthew.lau <matthew.lau@credera.co.uk>
2023-06-22 21:32:33 +0100

    feat: add data fetching

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, ReactElement, useEffect } from 'react'
+import { useContext, ReactElement, useEffect, useState } from 'react'
 import { AxiosResponse } from 'axios'
 import api from '../../utils/api'
 import { Store } from '../../store'
@@ -7,6 +7,11 @@ import SideNav from '../../components/side-nav'
 
 const DashboardPage = (): ReactElement => {
   const { state } = useContext(Store)
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [
+    currentUserMostRecentAlertThreshold,
+    setCurrentUserMostRecentAlertThreshold
+  ] = useState<number>(0)
 
   const getAlerts = async () => {
     try {
@@ -19,8 +24,31 @@ const DashboardPage = (): ReactElement => {
     }
   }
 
+  const getCurrentIndex = async () => {
+    try {
+      const { data }: AxiosResponse<any> = await api.get(
+        `${import.meta.env.VITE_API_BASE_URL}/api/records?count=0`
+      )
+      setCurrentIndex(data[0].index)
+    } catch (err) {
+      console.log('error!')
+    }
+  }
+  const getCurrentUserMostRecentAlertThreshold = async () => {
+    try {
+      const { data }: AxiosResponse<any> = await api.get(
+        `${import.meta.env.VITE_API_BASE_URL}/api/alerts/me`
+      )
+      setCurrentUserMostRecentAlertThreshold(data[0].index)
+    } catch (err) {
+      console.log('error!')
+    }
+  }
+
   useEffect(() => {
     getAlerts()
+    getCurrentIndex()
+    getCurrentUserMostRecentAlertThreshold()
   }, [])
 
   return (
@@ -54,7 +82,7 @@ const DashboardPage = (): ReactElement => {
                       className="w-10 h-10 text-white"
                     >
                       <path
-                        stroke-linecap="round"
+                        strokeLinecap="round"
                         stroke-linejoin="round"
                         d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
                       />
@@ -65,7 +93,7 @@ const DashboardPage = (): ReactElement => {
                       Current Index
                     </p>
                     <p className="text-white font-bold text-2xl inline-flex items-center space-x-2">
-                      <span>60</span>
+                      <span>{currentIndex}</span>
                       <span>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -76,7 +104,7 @@ const DashboardPage = (): ReactElement => {
                           className="w-6 h-6"
                         >
                           <path
-                            stroke-linecap="round"
+                            strokeLinecap="round"
                             stroke-linejoin="round"
                             d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
                           />
@@ -98,7 +126,7 @@ const DashboardPage = (): ReactElement => {
                       className="w-10 h-10 text-white"
                     >
                       <path
-                        stroke-linecap="round"
+                        strokeLinecap="round"
                         stroke-linejoin="round"
                         d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                       />
@@ -109,7 +137,7 @@ const DashboardPage = (): ReactElement => {
                       Alert Threshold
                     </p>
                     <p className="text-white font-bold text-2xl inline-flex items-center space-x-2">
-                      <span>35</span>
+                      <span>{currentUserMostRecentAlertThreshold}</span>
                       <span>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -120,7 +148,7 @@ const DashboardPage = (): ReactElement => {
                           className="w-6 h-6"
                         >
                           <path
-                            stroke-linecap="round"
+                            strokeLinecap="round"
                             stroke-linejoin="round"
                             d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
                           />
@@ -161,7 +189,7 @@ const DashboardPage = (): ReactElement => {
                       className="w-6 h-6"
                     >
                       <path
-                        stroke-linecap="round"
+                        strokeLinecap="round"
                         stroke-linejoin="round"
                         d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
                       />
@@ -211,7 +239,7 @@ const DashboardPage = (): ReactElement => {
                           className="w-5 h-5"
                         >
                           <path
-                            stroke-linecap="round"
+                            strokeLinecap="round"
                             stroke-linejoin="round"
                             d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
                           />
@@ -231,7 +259,7 @@ const DashboardPage = (): ReactElement => {
                           className="w-5 h-5"
                         >
                           <path
-                            stroke-linecap="round"
+                            strokeLinecap="round"
                             stroke-linejoin="round"
                             d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
                           />
@@ -251,7 +279,7 @@ const DashboardPage = (): ReactElement => {
                           className="w-5 h-5"
                         >
                           <path
-                            stroke-linecap="round"
+                            strokeLinecap="round"
                             stroke-linejoin="round"
                             d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
                           />
