36f7f3859dd5c5866dbce48b13ee1c8a79509ccf
matthew.lau <matthew.lau@credera.co.uk>
2023-06-23 20:01:06 +0100

    feat: added delete alert button

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -31,7 +31,15 @@ const DashboardPage = (): ReactElement => {
       setCurrentUserAlerts(data)
       setCurrentUserMostRecentAlert(data[0].index)
     } catch (err) {
-      setCurrentUserAlerts
+      console.log('error!')
+    }
+  }
+
+  const handleOnAlertDelete = async (id: string) => {
+    try {
+      await api.delete(`${import.meta.env.VITE_API_BASE_URL}/api/alerts/${id}`)
+      getCurrentUserAlerts()
+    } catch (err) {
       console.log('error!')
     }
   }
@@ -163,45 +171,8 @@ const DashboardPage = (): ReactElement => {
                       <td className="py-3 px-2">{alert.index}</td>
                       <td className="py-3 px-2">
                         <div className="inline-flex items-center space-x-3">
-                          <a href="" title="Edit" className="hover:text-white">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke-width="1.5"
-                              stroke="currentColor"
-                              className="w-5 h-5"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                stroke-linejoin="round"
-                                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                              />
-                            </svg>
-                          </a>
-                          <a
-                            href=""
-                            title="Edit password"
-                            className="hover:text-white"
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke-width="1.5"
-                              stroke="currentColor"
-                              className="w-5 h-5"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                stroke-linejoin="round"
-                                d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
-                              />
-                            </svg>
-                          </a>
-                          <a
-                            href=""
-                            title="Suspend user"
+                          <button
+                            onClick={() => handleOnAlertDelete(alert._id)}
                             className="hover:text-white"
                           >
                             <svg
@@ -218,7 +189,7 @@ const DashboardPage = (): ReactElement => {
                                 d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
                               />
                             </svg>
-                          </a>
+                          </button>
                         </div>
                       </td>
                     </tr>
