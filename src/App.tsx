80cf35d6a5a126bd9f02086b8a8a1953f5b83a9f
matthew.lau <matthew.lau@credera.co.uk>
2023-06-25 08:03:11 +0100

    feat: refactor routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,9 @@
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
-import { ReactElement, useContext } from 'react'
-import { StoreProvider, Store } from './store'
+import { RouterProvider } from 'react-router-dom'
+import { ReactElement } from 'react'
+import { StoreProvider } from './store'
 import AuthWrapper from './components/auth-wrapper'
+import { router } from './routes'
 import Alert from './components/alert'
-import HomePage from './pages/home'
-import SignUpPage from './pages/sign-up'
-import LoginPage from './pages/login'
-import DashboardPage from './pages/dashboard'
-import CreateAlertPage from './pages/create-alert'
-
-type PrivateRouteProps = {
-  component: React.ComponentType<any>
-}
-
-const PrivateRoute: React.FC<PrivateRouteProps> = ({
-  component: Component
-}) => {
-  const { state } = useContext(Store)
-  if (state.isAuthenticated) return <Component />
-  return <Navigate to="/login" />
-}
-
-const router = createBrowserRouter([
-  {
-    path: '/sign-up',
-    element: <SignUpPage />
-  },
-  {
-    path: '/login',
-    element: <LoginPage />
-  },
-  {
-    path: '/dashboard',
-    element: <PrivateRoute component={DashboardPage} />
-  },
-  {
-    path: '/create-alert',
-    element: <PrivateRoute component={CreateAlertPage} />
-  },
-  {
-    path: '/*',
-    element: <HomePage />
-  }
-])
 
 const App = (): ReactElement => {
   return (
---
+++ src/routes/index.tsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { Store } from '../store'
+import HomePage from '../pages/home'
+import SignUpPage from '../pages/sign-up'
+import LoginPage from '../pages/login'
+import DashboardPage from '../pages/dashboard'
+import CreateAlertPage from '../pages/create-alert'
+
+type PrivateRouteProps = {
+  component: React.ComponentType<any>
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  component: Component
+}) => {
+  const { state } = useContext(Store)
+  if (state.isAuthenticated) return <Component />
+  return <Navigate to="/login" />
+}
+
+export const router = createBrowserRouter([
+  {
+    path: '/sign-up',
+    element: <SignUpPage />
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/dashboard',
+    element: <PrivateRoute component={DashboardPage} />
+  },
+  {
+    path: '/create-alert',
+    element: <PrivateRoute component={CreateAlertPage} />
+  },
+  {
+    path: '/*',
+    element: <HomePage />
+  }
+])
