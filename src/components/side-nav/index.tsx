a3c079832091b26d29f76ebc32b1ef8d8823f8c8
matthew.lau <matthew.lau@credera.co.uk>
2024-03-08 08:25:05 +0000

    feat: component for side nav cta

--- src/components/side-nav/index.tsx
+++ src/components/side-nav/index.tsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react'
 import { Store } from '../../store'
 import { ActionType as AuthActionType } from '../../store/auth/action-types'
-import { Link } from 'react-router-dom'
 import DownloadIcon from '../icons/download-icon'
 import PlusIcon from '../icons/plus-icon'
 import LogoutIcon from '../icons/logout-icon'
@@ -22,78 +21,30 @@ const SideNav = () => {
         copy="Dashboard"
         description="Data overview"
       />
-      <Link
-        to="/create-alert"
-        className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group"
-      >
-        <div className="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-          <div>
-            <DocumentIcon />
-          </div>
-          <div>
-            <p className="font-bold text-base lg:text-lg text-slate-200 leading-4 group-hover:text-indigo-400">
-              Create Alert
-            </p>
-            <p className="text-slate-400 text-sm hidden md:block">
-              Create new alerts
-            </p>
-          </div>
-        </div>
-      </Link>
-      <Link
-        to="/export-data"
-        className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group"
-      >
-        <div className="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-          <div>
-            <DownloadIcon />
-          </div>
-          <div>
-            <p className="font-bold text-base lg:text-lg text-slate-200 leading-4 group-hover:text-indigo-400">
-              Export Data
-            </p>
-            <p className="text-slate-400 text-sm hidden md:block">
-              Export data to CSV
-            </p>
-          </div>
-        </div>
-      </Link>
-      <Link
-        to="/update-user"
-        className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group"
-      >
-        <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-          <div>
-            <CogwheelIcon />
-          </div>
-          <div>
-            <p className="font-bold text-base lg:text-lg text-slate-200 leading-4 group-hover:text-indigo-400">
-              Settings
-            </p>
-            <p className="text-slate-400 text-sm hidden md:block">
-              Edit settings
-            </p>
-          </div>
-        </div>
-      </Link>
-      <Link
-        to="/analysis-job"
-        className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group"
-      >
-        <div className="flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
-          <div>
-            <PlusIcon />
-          </div>
-          <div>
-            <p className="font-bold text-base lg:text-lg text-slate-200 leading-4 group-hover:text-indigo-400">
-              Analysis Job
-            </p>
-            <p className="text-slate-400 text-sm hidden md:block">
-              Create, and get analysis job
-            </p>
-          </div>
-        </div>
-      </Link>
+      <SideNavButton
+        url="/create-alert"
+        iconComponant={<DocumentIcon />}
+        copy="Create Alert"
+        description="Create new alerts"
+      />
+      <SideNavButton
+        url="/export-data"
+        iconComponant={<DownloadIcon />}
+        copy="Export Data"
+        description="Export data to CSV"
+      />
+      <SideNavButton
+        url="/update-user"
+        iconComponant={<CogwheelIcon />}
+        copy="Settings"
+        description="Edit settings"
+      />
+      <SideNavButton
+        url="/analysis-job"
+        iconComponant={<PlusIcon />}
+        copy="Analysis Job"
+        description="Create, and get analysis job"
+      />
       <button
         onClick={logout}
         className="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group"
