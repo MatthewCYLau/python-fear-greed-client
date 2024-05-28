26d4963d484230de97c32ce350ba094023a43a94
Matthew Lau <lau.cy.matthew@gmail.com>
2024-05-28 06:44:46 +0100

    feat: info icon

--- src/components/event-card/index.tsx
+++ src/components/event-card/index.tsx
@@ -1,5 +1,6 @@
 import { useState, FC } from 'react'
 import DeleteIcon from '../icons/delete-icon'
+import InfoIcon from '../icons/info-icon'
 
 interface Props {
   id: string
@@ -29,20 +30,7 @@ const EventCard: FC<Props> = ({ id, date, index, onDeleteHandler }) => {
           className="inline-flex space-x-2 items-center text-center"
           onClick={onClickHandler}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-            />
-          </svg>
+          <InfoIcon />
           <span>Info</span>
           {showToolTip && (
             <div
