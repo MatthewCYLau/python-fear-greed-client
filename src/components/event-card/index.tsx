c8d951dafe65467881b06413b4f34a7a9a9780a8
Matthew Lau <lau.cy.matthew@gmail.com>
2024-05-31 05:49:09 +0100

    chore: delete dead svg

--- src/components/event-card/index.tsx
+++ src/components/event-card/index.tsx
@@ -33,49 +33,7 @@ const EventCard: FC<Props> = ({ id, date, index, onDeleteHandler }) => {
           <InfoIcon />
           <span>Info</span>
           {showToolTip && (
-            <div
-              id="tooltip3"
-              role="tooltip"
-              className="z-20 -mt-50 w-64 absolute transition duration-150 ease-in-out left-20 ml-8 shadow-lg bg-gray-800 p-4 rounded"
-            >
-              <svg
-                className="absolute left-0 -ml-2 bottom-0 top-0 h-full"
-                width="9px"
-                height="16px"
-                viewBox="0 0 9 16"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g
-                  id="Page-1"
-                  stroke="none"
-                  strokeWidth="1"
-                  fill="none"
-                  fill-rule="evenodd"
-                >
-                  <g
-                    id="Tooltips-"
-                    transform="translate(-874.000000, -1029.000000)"
-                    fill="#2D3748"
-                  >
-                    <g
-                      id="Group-3-Copy-16"
-                      transform="translate(850.000000, 975.000000)"
-                    >
-                      <g
-                        id="Group-2"
-                        transform="translate(24.000000, 0.000000)"
-                      >
-                        <polygon
-                          id="Triangle"
-                          transform="translate(4.500000, 62.000000) rotate(-90.000000) translate(-4.500000, -62.000000) "
-                          points="4.5 57.5 12.5 66.5 -3.5 66.5"
-                        ></polygon>
-                      </g>
-                    </g>
-                  </g>
-                </g>
-              </svg>
+            <div className="z-20 -mt-50 w-64 absolute transition duration-150 ease-in-out left-20 ml-8 shadow-lg bg-gray-800 p-4 rounded">
               <p className="text-sm font-bold text-white pb-1">
                 Event Information
               </p>
