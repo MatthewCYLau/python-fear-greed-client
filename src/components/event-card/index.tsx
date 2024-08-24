3a6e37faea122f031ddd27a9ecc6a3c37ccceb5f
Matthew Lau <lau.cy.matthew@gmail.com>
2024-08-24 08:50:58 +0100

    feat: #8

--- src/components/event-card/index.tsx
+++ src/components/event-card/index.tsx
@@ -1,4 +1,4 @@
-import { useState, FC } from 'react'
+import { useState, FC, useRef, useEffect } from 'react'
 import DeleteIcon from '../icons/delete-icon'
 import InfoIcon from '../icons/info-icon'
 
@@ -11,10 +11,22 @@ interface Props {
 
 const EventCard: FC<Props> = ({ id, date, index, onDeleteHandler }) => {
   const [showToolTip, setShowToolTip] = useState<boolean>(false)
+  const ref = useRef<HTMLDivElement>(null)
 
   const onClickHandler = () => {
     setShowToolTip(!showToolTip)
   }
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent): void {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setShowToolTip(false)
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  })
   return (
     <div key={id} className="bg-black/60 to-white/5 rounded-lg">
       <div className="flex flex-row items-center">
@@ -32,18 +44,21 @@ const EventCard: FC<Props> = ({ id, date, index, onDeleteHandler }) => {
         >
           <InfoIcon />
           <span>Info</span>
-          {showToolTip && (
-            <div className="z-20 -mt-50 w-64 absolute transition duration-150 ease-in-out left-20 ml-8 shadow-lg bg-gray-800 p-4 rounded">
-              <p className="text-sm font-bold text-white pb-1">
-                Event Information
-              </p>
-              <p className="text-xs leading-4 text-white pb-3 ">
-                Alert was triggered on <span className="font-bold">{date}</span>{' '}
-                when index was <span className="font-bold">{index}</span>
-              </p>
-            </div>
-          )}
         </button>
+        {showToolTip && (
+          <div
+            ref={ref}
+            className="z-20 -mt-30 w-64 absolute transition duration-150 ease-in-out left-20 ml-4 shadow-lg bg-gray-800 p-4 rounded"
+          >
+            <p className="text-sm font-bold text-white pb-1">
+              Event Information
+            </p>
+            <p className="text-xs leading-4 text-white pb-3 ">
+              Alert was triggered on <span className="font-bold">{date}</span>{' '}
+              when index was <span className="font-bold">{index}</span>
+            </p>
+          </div>
+        )}
         <button onClick={onDeleteHandler} className="hover:text-white">
           <DeleteIcon />
         </button>
