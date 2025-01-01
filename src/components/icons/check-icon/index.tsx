763955d704db269e724d77cb73fb1f3ec87412fd
Matthew Lau <lau.cy.matthew@gmail.com>
2025-01-01 12:28:23 +0000

    feat: stroke colour green

--- src/components/icons/check-icon/index.tsx
+++ src/components/icons/check-icon/index.tsx
@@ -7,13 +7,11 @@ const CheckIcon: FC = () => {
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth="1.5"
-      stroke="currentColor"
-      className="w-6 h-6 group-hover:text-indigo-400"
+      className="w-6 h-6 stroke-green-400"
     >
-      <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="1.5" />
+      <circle cx="12" cy="12" r="10" strokeWidth="1.5" />
       <path
         d="M8.5 12.5L10.5 14.5L15.5 9.5"
-        stroke="currentColor"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
