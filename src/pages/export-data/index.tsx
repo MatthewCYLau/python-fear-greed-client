ef76c0b5cca5887940e7e245dbf562d481ce9a42
Matthew Lau <lau.cy.matthew@gmail.com>
2025-01-29 20:59:38 +0000

    feat: re-use dropdown component

--- src/pages/export-data/index.tsx
+++ src/pages/export-data/index.tsx
@@ -10,6 +10,7 @@ import Layout from '../../components/layout'
 
 import 'react-datepicker/dist/react-datepicker.css'
 import Loader from '../../components/loader'
+import DropdownButton from '../../components/dropdown-button'
 
 const chartTypes: ChartType[] = [
   ChartTypeValues.SCATTER,
@@ -209,13 +210,12 @@ const ExportDataPage = (): ReactElement => {
                 className="absolute w-full right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1 z-2"
               >
                 {chartTypes.map((n) => (
-                  <button
-                    key={n}
-                    onClick={() => dropdownItemOnClickHandler(n)}
-                    className="w-full block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md"
-                  >
-                    {capitaliseFirstLetter(n)}
-                  </button>
+                  <DropdownButton
+                    copy={capitaliseFirstLetter(n)}
+                    dropdownItemOnClickHandler={() =>
+                      dropdownItemOnClickHandler(n)
+                    }
+                  />
                 ))}
               </div>
             )}
