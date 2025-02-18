8d5d76bb070899181cd0d3e21254d87690778b2b
Matthew Lau <lau.cy.matthew@gmail.com>
2025-02-18 20:05:59 +0000

    feat: onBlur upper case

--- src/pages/cumulative-returns/index.tsx
+++ src/pages/cumulative-returns/index.tsx
@@ -134,6 +134,12 @@ const CumulativeReturnsPage = (): ReactElement => {
               className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               value={formValues.stockSymbol}
               onChange={(e) => onChange(e)}
+              onBlur={() =>
+                setFormValues({
+                  ...formValues,
+                  stockSymbol: formValues.stockSymbol.toUpperCase()
+                })
+              }
             />
           </div>
           <div className="mb-6">
