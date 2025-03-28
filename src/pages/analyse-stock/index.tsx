cc7cb3e21447fa51953898039cee48b556581794
Matthew Lau <lau.cy.matthew@gmail.com>
2025-03-28 12:41:23 +0000

    feat: invoke API

--- src/pages/analyse-stock/index.tsx
+++ src/pages/analyse-stock/index.tsx
@@ -1,6 +1,8 @@
 import { ReactElement, useState, ChangeEvent } from 'react'
 
 import Layout from '../../components/layout'
+import { AxiosResponse } from 'axios'
+import api from '../../utils/api'
 
 interface Values {
   stockSymbol: string
@@ -13,6 +15,19 @@ const AnalyseStockPage = (): ReactElement => {
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleAnalyseStock = async () => {
+    try {
+      const { data }: AxiosResponse<any> = await api.get(
+        `${import.meta.env.VITE_API_BASE_URL}/api/analysis?stock=${
+          formValues.stockSymbol
+        }&targetFearGreedIndex=50&targetPeRatio=25`
+      )
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
@@ -36,10 +51,16 @@ const AnalyseStockPage = (): ReactElement => {
               className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
               value={formValues.stockSymbol}
               onChange={(e) => onChange(e)}
+              onBlur={() =>
+                setFormValues({
+                  ...formValues,
+                  stockSymbol: formValues.stockSymbol.toUpperCase()
+                })
+              }
             />
           </div>
           <button
-            onClick={() => console.log('Analyse stock')}
+            onClick={handleAnalyseStock}
             className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
           >
             Analyse Stock
