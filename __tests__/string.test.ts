b5dda072066e1a92d35d5ee40e2b833af6bbb5d1
Matthew Lau <lau.cy.matthew@gmail.com>
2026-02-07 00:07:48 +0000

    feat: tests

--- __tests__/string.test.ts
+++ __tests__/string.test.ts
@@ -1,6 +1,7 @@
 import {
   capitaliseFirstLetter,
-  formatAmountTwoDecimals
+  formatAmountTwoDecimals,
+  generateColours
 } from '../src/utils/string'
 
 test('Capitalise first letter', () => {
@@ -10,4 +11,9 @@ test('Capitalise first letter', () => {
 test('Format amount to two decimals', () => {
   expect(formatAmountTwoDecimals('123')).toEqual('123.00')
   expect(formatAmountTwoDecimals('123.1')).toEqual('123.10')
+})
+
+test('Generates correct array of rgb string', () => {
+  expect(generateColours(1).length).toEqual(1)
+  expect(generateColours(1)[0].split(',').length).toEqual(3)
 })
